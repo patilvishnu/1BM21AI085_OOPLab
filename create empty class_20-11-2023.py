b78545925f7e05ebf4f9767c3bdddf6ca6078d8f
patilvishnu <138456159+patilvishnu@users.noreply.github.com>
2023-11-20 11:40:59 +0530

    Create create empty class_20-11-2023.py

---
+++ create empty class_20-11-2023.py
@@ -0,0 +1,13 @@
+class students():
+    pass
+class marks():
+    pass
+
+stu1=students()
+stu2=students()
+mar1=marks()
+mar2=marks()
+print(isinstance(stu1,students))
+print(isinstance(stu2,students))
+print(isinstance(stu1,marks))
+print(isinstance(mar1,marks))
